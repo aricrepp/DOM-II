04633dda2cc11b95496e62b7b99247d424b0bf6e
Aric Repp <aricmrepp@gmail.com>
2020-04-10 21:49:33 -0400

    Some events not working, submitting what I have

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 const firstElement = document.querySelector(".intro img");
 const secondElement = document.querySelectorAll(".img-content img");
 const thirdElement = document.querySelector(".text-content h2");
+const fourthElement = document.querySelector("footer");
+const fifthElement = document.querySelectorAll("a");
 
 
 firstElement.addEventListener("mouseenter", function(){
@@ -11,30 +13,41 @@ firstElement.addEventListener("mouseenter", function(){
 firstElement.addEventListener("mouseleave", function(){
     firstElement.style.transform = "scale(1)";
     firstElement.style.transition = "transform 0.5s";
+    firstElement.style.transition = "none";
+    firstElement.style.transform = "none";
 });
 
 secondElement.forEach((El) => {
     El.addEventListener('mouseenter', (event) => {
         event.target.style.transform = "scale(1.1)";
         event.target.style.transition = "transform 1s";
-        event.target.style.zIndex = "0";
-        event.target.style.opacity = "0.99";
+        event.target.style.zIndex = "1"
     });
 });
 secondElement.forEach((El) => {
     El.addEventListener('mouseleave', (event) => {
         event.target.style.transform = "scale(1)";
-        event.target.style.transition = "transform 1s";
-        event.target.style.zIndex = "0";
+        event.target.style.transition = "transform 0.2s";
+        event.target.style.zIndex = "-1";
+        event.target.style.transition = "none";
+        event.target.style.transform = "none";
     });
 });
 
-thirdElement.addEventListener("mouseenter", function(){
-    thirdElement.style.transformOrigin = "top left";
-    thirdElement.style.transform = "scale(1.5,1)";
-    thirdElement.style.transition = "transform 0.5s";
+thirdElement.addEventListener("mouseover", function(event){
+    event.target.style.transformOrigin = "top left";
+    event.target.style.transform = "scale(1.5,1)";
+    event.target.style.transition = "transform 0.5s";  
+
 });
 thirdElement.addEventListener("mouseleave", function(){
     thirdElement.style.transform = "scale(1)";
     thirdElement.style.transition = "transform 0.5s";
+    thirdElement.style.zIndex = "-1";
 });
+
+fourthElement.addEventListener("mousemove", function(){
+    fourthElement.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+});
+
+fifthElement.addEventListener()
